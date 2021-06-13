423b5bda0847bfafca68033ab1fb8e121dd541f3
OsamaBACS <osama.solution@gmail.com>
2021-06-13 14:45:01 +0300

    LearnBindings

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -2,10 +2,26 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-course',
-  templateUrl: './course.component.html',
+  // templateUrl: './course.component.html',
+  // use Pipe Operator
+  template: `
+    {{ course.title | uppercase }} <br/>
+    {{ course.students | number }} <br/>
+    {{ course.rating | number }} <br/>
+    {{ course.price }} <br/>
+    {{ course.releaseDate }} <br/>
+  `,
   styleUrls: ['./course.component.css']
 })
 export class CourseComponent implements OnInit {
+
+  course = {
+    title: "The Complete Angular Course",
+    rating: 4.9745,
+    students: 30123,
+    price: 190.95,
+    releaseDate: new Date(2016, 3, 1)
+  }
 
   constructor() { }
 
--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -10,11 +10,41 @@ import { CoursesService } from './courses.service';
         {{ course }}
       </li>
     </ul>
-  `,
+    <img src="{{ imageUrl }}" />
+    <hr>
+    <img [src]="imageUrl" />
+    <hr>
+    <table class="table table-bordered">
+      <tr>
+        <td [attr.colspan]="colSpan">Osama</td>
+      </tr>
+    </table>
+    <hr>
+    <input (keyup.enter)="onKeyUp($event)" />
+    <hr>
+    <!-- <input #email (keyup.enter)="onKeyUp2(email.value)" /> -->
+    <hr>
+    <input [(ngModel)]="email" (keyup.enter)="onKeyUp3()" />
+    <hr>
+    <div (click)="onDivClicked()">
+      <button class="btn btn-primary"
+              [class.active]="isActive"
+              [style.backgroundColor]='isActive ? "blue" : "white"'
+              (click)="onSave($event)">
+            OK
+      </button>
+    </div>
+  `,                                 // Class Binding         Style Binding                                         Event Binding
+
+  styles: ['img {width: 200px; height: 200px}']
 })
 export class CoursesComponent {
   title = 'List of Courses';
   courses;
+  imageUrl = "assets/images/p1.jpg";
+  colSpan = 2;
+  isActive = false;
+  email: any;
 
   constructor(service: CoursesService) {
     this.courses = service.getCourses();
@@ -22,5 +52,23 @@ export class CoursesComponent {
 
   getTitle() {
     return this.title;
+  }
+
+  onSave($event: any){
+    $event.stopPropagation(); // To Stop Event Bubbling
+    console.log('Button Was Clicked', $event);
+  }
+  onDivClicked(){
+    console.log('Div Was Clicked');
+  }
+  onKeyUp($event: any){
+    console.log('Enter was Pressed');
+    console.log($event.target.value);
+  }
+  onKeyUp2(email: any){
+    console.log(email);
+  }
+  onKeyUp3(){
+    console.log(this.email);
   }
 }
